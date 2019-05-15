4bca001478657cb77a77d6c059cb033e59bc22e2
parastoo moslemi <moslemi.parastoo@gmail.com>
2019-05-15 14:39:41 -0700

    add gittest and shop to dev

---
+++ shop.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+    <meta charset="utf-8">
+
+    <title>  kodakam </title>
+
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+
+    <meta name="title" content="وب سایت کودکان">
+    <meta name="keywords" content="وب سایت کودکانه کودک نقاشی ورزش عکس">
+    <meta name="description" content="وبسایتی برای بازی و سرگرمی کودکان">
+
+    <link rel="shortcut icon" href="<?=base_url()?>assets/users.ico">
+    <link rel="icon" type="image/icon" href="<?=base_url()?>assets/users.ico"/>
+
+</head>
+<body>
+    <h1>welcome to shop</h1>
+</body>
+</html>
